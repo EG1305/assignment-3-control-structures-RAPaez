72b3d7121115183d4a2fb41dfb15de0417cff804
Ricardo <105883017+RAPaez@users.noreply.github.com>
2025-02-09 16:46:09 -0600

    Add files via upload

---
+++ SpeedDetector2.java
@@ -0,0 +1,30 @@
+// Ricardo Paez Assignment 2 - Speed Detector
+
+import javax.swing.JOptionPane;
+
+public class SpeedDetector2 {
+	public static void main(String[] args) {
+
+		// Assigning variables
+		String input;
+		int speed;
+
+		// Getting the users speed through the input dialog
+		input = JOptionPane.showInputDialog("Please enter your speed:");
+		speed = (Integer.parseInt(input));
+
+		// Printing out the users speed
+		JOptionPane.showMessageDialog(null, "Your speed is " + speed);
+
+		// Shows a warning message only if the speed is greater than 60
+		if (speed > 60) {
+			JOptionPane.showMessageDialog(null, "Slow down! drive safe.", "Ticket Warning",
+					JOptionPane.WARNING_MESSAGE);
+		}
+		// Congratulates the driver if they are going at least 60 or below 
+		else {
+			JOptionPane.showMessageDialog(null, "Congratulations! You are a safe driver.");
+		}
+
+	}
+}
